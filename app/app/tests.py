bd40fd93eb8ffdc3ceb801a822b2e83819ba5bb1
stryjko <maryan.partyka@gmail.com>
2023-04-25 14:23:42 +0300

    added postgresql config

---
+++ app/app/tests.py
@@ -0,0 +1,19 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+
+        self.assertEquals(res, 11)
+
+    def test_subtract_numbers(self):
+        res = calc.subtract(10, 15)
+
+        self.assertEquals(res, 5)
