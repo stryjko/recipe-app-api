fc8bfd49e3994ca4e7dfdae4cd4c750d4c347637
stryjko <maryan.partyka@gmail.com>
2023-05-01 14:34:49 +0300

    Added aPI docs with Swagger

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -39,7 +39,7 @@ class ModelTests(TestCase):
 
     def test_create_superuser(self):
         """Test creating a superuser."""
-        user = get_user_model().objects.create_user(
+        user = get_user_model().objects.create_superuser(
             'test@example.com',
             'test123',
         )
