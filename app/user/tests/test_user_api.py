013062c0e23894823b9435d3d907122f29459faa
stryjko <maryan.partyka@gmail.com>
2023-05-03 14:35:51 +0300

    User API, fixed flake8

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -146,4 +146,3 @@ class PrivateUserApiTests(TestCase):
         self.assertEqual(self.user.name, payload['name'])
         self.assertTrue(self.user.check_password(payload['password']))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-
